8799fdb956a4d1133307f85d03be57a49e394363
Jordan Blakey <jordan@jordanblakey.com>
2017-11-27 12:09:39 -0600

    init

---
+++ src/xray.js
@@ -0,0 +1,8 @@
+// X-Ray
+import Xray from 'x-ray';
+
+const x = Xray();
+console.log(Xray);
+console.log(Xray());
+
+
---
+++ xray.bundle.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('x-ray');
+
+var x = Xray(); // X-Ray
